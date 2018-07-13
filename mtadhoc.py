723202b867fbe625a5aff283b1db8bb8ef027e5b
Wonkey <25494365+wonkeymonkey@users.noreply.github.com>
2018-07-13 19:33:03 -0400

    mtadhoc.py

--- mtadhoc.py
+++ mtadhoc.py
@@ -1,17 +1,9 @@
+#!/usr/bin/env python3
 from json import loads, dumps
 from multiprocessing import Process, Queue
 from math import sqrt, pi, log
 from time import sleep, time
-import sys
 import dill
-
-try:
-    from secrets import SystemRandom
-except ImportError:
-    from random import SystemRandom
-sr = SystemRandom()
-rchoice = sr.choice
-
 
 from mtstations import Station
     
@@ -25,14 +17,14 @@ class Airspace:
         return d
 
     #This is very much NOT representative of the real world
-    @staticmethod
-    def rxpower(d, Ptx, Dtx, Drx):
-       _ = Ptx + Dtx + Drx + 20 * log( (WL / (4*pi*d)) , 10)
-       return _
+##    @staticmethod
+##    def rxpower(d, Ptx, Dtx, Drx):
+##       _ = Ptx + Dtx + Drx + 20 * log( (WL / (4*pi*d)) , 10)
+##       return _
 
     @staticmethod
     def threadp(frozenStation, queues, processQueue, stopQueue):
-
+        """This is the function called as a seperate process for each station"""
         station = dill.loads(frozenStation)
         del frozenStation
         station.unfreeze(queues)
@@ -71,6 +63,7 @@ class Airspace:
 
 
     def freeze(self):
+        """freeze and then dill-pickle each station/node, then move them to frozenStations. After this we should be ready for going multiprocess"""
         self.frozenStations.clear()
         for station in self.stations:
             station.freeze()
@@ -79,6 +72,7 @@ class Airspace:
 
 
     def unfreeze(self):
+        """ unfreeze each station, that way we can interact with them on the main thread """
         self.stations.clear()
         for station, sid in self.frozenStations:
             self.stations.append( dill.loads(station) )
@@ -88,7 +82,7 @@ class Airspace:
     def __call__(self, seconds=120):
 
         self.freeze()
-
+        #Prepare queues for each process and station, prepare multithreads
         threads = len(self.frozenStations)
         stopq = Queue()
         procqueues = []
@@ -99,6 +93,7 @@ class Airspace:
             self.outqdict[sid] = oq
             procqueues.append(Queue())
             procs.append( Process(target=self.threadp, args=(fs, (iq, oq), procqueues[-1], stopq)) )
+        #Start the station's threads
         [ p.start() for p in procs ]
         
         try:
@@ -111,7 +106,7 @@ class Airspace:
         print("Stopping")
 
         while len([p for p in procs if p.is_alive()]) > 0:
-            stopq.put("STOP")
+            stopq.put("STOP") #There's litterally no reason to put "STOP" multiple times if everything goes as expected
             print("Waiting for threads to stop...")
             sleep(1)
 
@@ -125,6 +120,7 @@ class Airspace:
                 
         
     def packetHandler(self):
+        """check for new outbound packets, and send them on their way using 'send'"""
         for sid, q in self.outqdict.items():
             if not q.empty():
                 packet = q.get()
@@ -132,6 +128,7 @@ class Airspace:
                 
                 
     def send(self, packet, sid):
+        """add packets to all nodes within broadcasting range of the sending station, also: here is were any debugging messages are printed"""
         d = loads(packet)
         dst = d.get('dst')
         typ = d.get('typ')
@@ -168,7 +165,7 @@ class Airspace:
                 destsid = dest.sid
                 destpos = pdict[destsid]
                 if distance(srcpos, destpos) < self.maxrange:
-                    rxpower = 1
+                    rxpower = 1 #In the future rxpower will be a function of distance, which will impact packet delivery
                     rdict[srcsid].append((destsid, rxpower))
         
 
@@ -178,8 +175,23 @@ class Airspace:
         pass
 
 
-a = Airspace()
-#a.makeStation([(0,0),(100,100), (200,200)])
-a.makeStation([(0,0), (100,100)])
-a(120)
+if __name__ == "__main__":
+
+    import sys
+    if "idlelib.run" in sys.modules:
+        print("Can't multiprocess while running in IDLE! Run this from a terminal", file=sys.stderr)
+
+    if (sys.version_info.major < 3) or (sys.version_info.minor < 6):
+        print("Oh yeah, also I used that fancy new string formatting in Python3.6, It looks like this is an older version that that", file=sys.stderr)
+        print("Those print(f\"{variable}\") are the only new feature I used, I think, but either way It probably won't work with the version you're using", file=sys.stderr)
+    
+    a = Airspace()
+
+    #The transmit limit is about 100, so these 4 nodes can't all transmit to each other
+    a.makeStation([(0,0),(100,100), (200,200), (300,300)])
+
+    print({ k:[sid for sid,rxpower in p] for k,p in a.rdict.items() })
+    sleep(2)
+      
+    a(300) #Run for 5 minutes
 
